f66ba3635aeec97a677e4fe54787f8d54deb3d2e
CHRIS BIGLAND <christopherbigland@gmail.com>
2022-12-21 12:48:30 +0000

    cleanedCatFacts now returns an array of objects, each with the fact and a unique ID

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
 import styles from "./App.module.scss";
 import { useState } from "react";
+import { cleanup } from "@testing-library/react";
 
 const App = () => {
   const [facts, setFacts] = useState([]);
 
   const API_URL = "https://catfact.ninja/facts";
+  const factsArr = [];
+  let factIdNo = 0;
 
-  const cleanedCatFacts = (fact) => {
-    const factsArr = [];
+  const cleanCatFacts = (fact, factIdNo) => {
+    // fact is the object
+    let cleanedFact;
     Object.keys(fact).forEach((key) => {
+      //key is the key item in our new array here
       if (key.includes("fact")) {
-        factsArr.push(fact[key]);
+        // factsArr.push(fact[key]);
+        cleanedFact = { fact: fact[key], factId: factIdNo }; // sets the value of the key to this variable
       }
     });
-    console.log(factsArr);
+    // console.log(factsArr);
+    // return factsArr;
+    console.log(cleanedFact);
+    return cleanedFact;
   };
-  // cleanedCatFacts removes the 'length' from the fact and adds in a unique ID. It gets all the keys from the fact and then adds the key to the facts array if it is called 'fact'.
+  // cleanCatFacts removes the 'length' from the fact, places it into a new array and I want it to add in a unique ID. It gets all the keys from the fact and then adds the key to the facts array if it is called 'fact' (basically adds all the facts into an array).
+
+  // ISSUE HERE THAT I AM CREATING MULTIPLE ARRAYS?
 
   const getCatFact = () => {
     fetch("https://catfact.ninja/facts")
       .then((response) => response.json())
       .then((jsonResponse) => {
         // console.log(jsonResponse.data)
-        cleanedCatFacts(jsonResponse.data[0]);
-        setFacts(jsonResponse.data);
+        const jsonCatFacts = jsonResponse.data;
+        console.log("jsonCatFacts is", jsonCatFacts); // an array of objects, each with a 'fact' and a 'length' key.
+
+        const cleanedCatFacts = jsonCatFacts.map((object) => {
+          // map over the array of objects. Each object gets passed into the cleanCatFacts function where an array of its keys are created (will be 2 items).
+          factIdNo = factIdNo + 1;
+
+          return cleanCatFacts(object, factIdNo); // each time cleanCatFacts() is run, I get returned an array containing just the fact. I WANT AN OBJECT instead
+        });
+        // const cleanedCatFacts = cleanCatFacts(jsonCatFacts);
+        console.log(cleanedCatFacts);
+
+        setFacts(cleanedCatFacts);
         console.log("facts are", facts);
       })
       .catch((error) => {
